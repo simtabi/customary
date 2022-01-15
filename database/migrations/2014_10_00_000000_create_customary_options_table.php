f4422d87c2cbfd5b84061f54950a6ad2f3750f74
Imani Manyara <imani@simtabi.com>
2022-01-15 12:51:06 -0500

    Bug fixes

--- database/migrations/2014_10_00_000000_create_customary_options_table.php
+++ database/migrations/2014_10_00_000000_create_customary_options_table.php
@@ -16,12 +16,13 @@ class CreateCustomaryOptionsTable extends Migration
         if (!Schema::hasTable('customary_options')) {
             Schema::create('customary_options', function (Blueprint $table) {
                 $table->uuid('id')->primary()->index()->unique();
-                $table->uuidMorphs('ownerable');
                 $table->string('group')->default('default');
                 $table->string('sub_group')->nullable()->default('default');
                 $table->string('key');
                 $table->text('value')->nullable();
                 $table->text('description')->nullable();
+                $table->integer('order')->default(0);
+                $table->uuidMorphs('ownerable');
                 $table->string('locale')->nullable()->index();
                 $table->unique(['key', 'locale']);
                 $table->timestamps();
